bf934544a52a70cbb2b56bb42347f6033ea0c2f2
Leonardo Requena <leoreq@gmail.com>
2015-08-24 05:55:59 +0000

    added the drink naming functionanily and fixed the iterations so that drink list got reseted after each order

--- pirateBartender.py
+++ pirateBartender.py
@@ -1,6 +1,8 @@
 import random
 drinkStyle={}
 clientDrink=[]
+drinkName={}
+
 questions = {
     "strong": "Do ye like yer drinks strong?",
     "salty": "Do ye like it with a salty tang?",
@@ -16,6 +18,8 @@ ingredients = {
     "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
     "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
 }
+
+piratestuff=['bucaneer','captain','chandler','bucaneer','corsair','coxswain']
 logo=[" The Pirate Bartender Welcomes Yall.",
  "||-.,.,.,.,,.,.,...-' ;",
  "||     _          _    |",
@@ -39,8 +43,8 @@ def printPirate():
         
 def askDrink():
     """This funciton will ask for style of drink, and store answer"""  
-    
-    print("Answer True or False to the following questions so that we can make err self a drink. Yarrrrr")
+    print ""
+    print("Ill make ya some questions so that we can make err self a nice drink. Yarrrrr")
     for style in questions:
         isBoolean=True
         while isBoolean:
@@ -55,20 +59,46 @@ def askDrink():
 
 def mixDrink():
     """This funciton will take the drinking style , use the taste key to access ingredient dictionary and randomly mix the ingredientes for the coctail"""
+    #clientDrink=[]
+    print ""
     print("Lets start mixn ya drinkss")
     for style in drinkStyle:
         if drinkStyle[style]=="YES":
             ingredient=random.choice(ingredients[style])
             clientDrink.append(ingredient)
-
+            drinkName[style]=[ingredient]
+            
+def drinkNaming():
+    """This function will name the drink"""
+    #First the dominating taste
+    domTaste=random.choice(drinkName.keys())
+    #Piraty stuff
+    piratyName=random.choice(piratestuff)
+    #Dominant Drink
+    VirginCheck=drinkName.get("strong",["virgin"])
+    dList=["rum","whisky","gin","virgin"]
+    for alcohol in dList:
+        if alcohol in VirginCheck[0].split():
+            dominantDrink=alcohol
+    print ""
+    print("And this drink will be known as ")
+    print (" {} {} {}").format(domTaste,piratyName,dominantDrink)
+    
 def serveDrink():
     """This function will explain the drink"""
-    print (" Drink is Ready.")
-    print(clientDrink)
+    print ""
+    print (" Drink is Ready!!!")
+    print("What youll be having is")
+    for ingredient in clientDrink:
+        print("A {}").format(ingredient)
+    drinkNaming()
+    clientDrink[:]=[]
+    #drinkName={}
     
 def anotherDrink():
     understandInput=True
     while understandInput:
+        print ""
         another=raw_input("Want another Drink?")
         if (another.upper()=="YES") or (another.upper()=="NO"):
             if (another.upper()=="YES"):
